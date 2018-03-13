856e920bf9cb9d87a2c8976562a975a2a1357029
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2018-03-13 09:30:54 -0400

    Card Class Finished

---
+++ ShuangCards/src/Card.java
@@ -0,0 +1,53 @@
+
+public class Card {
+
+	private String rank;
+	private String suit;
+	private int pointvalue;
+
+	public Card(String rank, String suit, int pointValue)
+	{
+		this.rank = rank;
+		this.suit = suit;
+		this.pointvalue = pointValue;
+	}
+	
+	public boolean equals(Card otherCard)
+	{
+		if (this.getPointvalue()==otherCard.getPointvalue())
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public int getPointvalue() {
+		return pointvalue;
+	}
+
+	public void setPointvalue(int pointvalue) {
+		this.pointvalue = pointvalue;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cards [getRank()=" + getRank() + ", getSuit()=" + getSuit() + ", getPointvalue()=" + getPointvalue()
+				+ "]";
+	}
+}
